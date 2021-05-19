2bdd28a41324c6d7fa581dd13dc82ad15c16f951
Charlie <charlie_mort@outlook.com>
2021-05-19 20:37:09 +0100

    Added Client Demo

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
-const e = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'));
+});
+
 const server = http.createServer(app);
 const io = socketIO(server, {
     cors: {
